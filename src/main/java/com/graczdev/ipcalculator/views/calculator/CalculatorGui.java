35b7385a93a334bed9ebf5aaed472b1c23cabcc6
Rollczi <ndejlich5@gmail.com>
2021-11-02 02:32:40 +0100

    Initial commit

---
+++ src/main/java/com/graczdev/ipcalculator/views/calculator/CalculatorGui.java
@@ -0,0 +1,27 @@
+package com.graczdev.ipcalculator.views.calculator;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("About")
+@Route(value = "/")
+public class CalculatorGui extends VerticalLayout {
+
+    public CalculatorGui() {
+        setSpacing(false);
+
+        add(new H2("Tutaj będzie nasz kalkulator :)"));
+        add(new Paragraph("Zaczynamy prace nad naszym algorytmem 🤗"));
+        add(new Button("Oblicz"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
---
+++ src/test/java/com/graczdev/ipcalculator/IpcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graczdev.ipcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpcalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
